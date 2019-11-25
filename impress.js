3a17433c247115ba5aecf24ea2c3c6bad11a341f
Annie Decker <anniedecker@ou.edu>
2019-11-25 10:27:10 -0600

    second commit

--- impress.js
+++ impress.js
@@ -3,56 +3,34 @@
 
  * impress.js
 
- *
-
- * impress.js is a presentation tool based on the power of CSS3 transforms and transitions
-
- * in modern browsers and inspired by the idea behind prezi.com.
-
- *
-
  * MIT Licensed.
 
- *
-
  * Copyright 2011 Bartek Szopka (@bartaz)
+ 
+ * Used by Annie Decker, Paula Tran, and Maia Stokes for MIS 3033 project.
 
  */
 
-
-
 (function (document, window) {
-
-
-
+    
     // HELPER FUNCTIONS
 
-
-
     var pfx = (function () {
-
-
-
+        
         var style = document.createElement('dummy').style,
 
             prefixes = 'Webkit Moz O ms Khtml'.split(' '),
 
             memory = {};
-
-
-
+        
         return function (prop) {
 
             if (typeof memory[prop] === "undefined") {
-
-
-
+                
                 var ucProp = prop.charAt(0).toUpperCase() + prop.substr(1),
 
                     props = (prop + ' ' + prefixes.join(ucProp + ' ') + ucProp).split(' ');
-
-
-
+                
                 memory[prop] = null;
 
                 for (var i in props) {
@@ -66,31 +44,21 @@
                     }
 
                 }
-
-
-
+                
             }
-
-
-
+            
             return memory[prop];
 
         }
 
-
-
     })();
-
-
-
+    
     var arrayify = function (a) {
 
         return [].slice.call(a);
 
     };
-
-
-
+    
     var css = function (el, props) {
 
         var key, pkey;
@@ -114,17 +82,13 @@
         return el;
 
     }
-
-
-
+    
     var byId = function (id) {
 
         return document.getElementById(id);
 
     }
-
-
-
+    
     var $ = function (selector, context) {
 
         context = context || document;
@@ -132,9 +96,7 @@
         return context.querySelector(selector);
 
     };
-
-
-
+    
     var $$ = function (selector, context) {
 
         context = context || document;
@@ -142,17 +104,13 @@
         return arrayify(context.querySelectorAll(selector));
 
     };
-
-
-
+    
     var translate = function (t) {
 
         return " translate3d(" + t.x + "px," + t.y + "px," + t.z + "px) ";
 
     };
-
-
-
+    
     var rotate = function (r, revert) {
 
         var rX = " rotateX(" + r.x + "deg) ",
@@ -160,43 +118,29 @@
             rY = " rotateY(" + r.y + "deg) ",
 
             rZ = " rotateZ(" + r.z + "deg) ";
-
-
-
+        
         return revert ? rZ + rY + rX : rX + rY + rZ;
 
     };
-
-
-
+    
     var scale = function (s) {
 
         return " scaleX(" + s.x + ") scaleY(" + s.y + ") scaleZ(" + s.z + ") ";
 
     }
-
-
-
+    
     // CHECK SUPPORT
-
-
-
+    
     var ua = navigator.userAgent.toLowerCase();
 
     var impressSupported = (pfx("perspective") != null) &&
 
         (ua.search(/(iphone)|(ipod)|(ipad)|(android)/) == -1);
-
-
-
+    
     // DOM ELEMENTS
-
-
-
+    
     var impress = byId("impress");
-
-
-
+    
     if (!impressSupported) {
 
         impress.className = "impress-not-supported";
@@ -208,15 +152,11 @@
         impress.className = "";
 
     }
-
-
-
+    
     var canvas = document.createElement("div");
 
     canvas.className = "canvas";
-
-
-
+    
     arrayify(impress.childNodes).forEach(function (el) {
 
         canvas.appendChild(el);
@@ -224,23 +164,15 @@
     });
 
     impress.appendChild(canvas);
-
-
-
+    
     var steps = $$(".step", impress);
-
-
-
+    
     // SETUP
 
     // set initial values and defaults
-
-
-
+    
     document.documentElement.style.height = "100%";
-
-
-
+    
     css(document.body, {
 
         height: "100%",
@@ -248,9 +180,7 @@
         overflow: "hidden"
 
     });
-
-
-
+    
     var props = {
 
         position: "absolute",
@@ -262,9 +192,7 @@
         transformStyle: "preserve-3d"
 
     }
-
-
-
+    
     css(impress, props);
 
     css(impress, {
@@ -278,9 +206,7 @@
     });
 
     css(canvas, props);
-
-
-
+    
     var current = {
 
         translate: { x: 0, y: 0, z: 0 },
@@ -290,9 +216,7 @@
         scale: { x: 1, y: 1, z: 1 }
 
     };
-
-
-
+    
     steps.forEach(function (el, idx) {
 
         var data = el.dataset,
@@ -330,21 +254,15 @@
                 }
 
             };
-
-
-
+        
         el.stepData = step;
-
-
-
+        
         if (!el.id) {
 
             el.id = "step-" + (idx + 1);
 
         }
-
-
-
+        
         css(el, {
 
             position: "absolute",
@@ -360,21 +278,13 @@
             transformStyle: "preserve-3d"
 
         });
-
-
-
+        
     });
-
-
-
+    
     // making given step active
-
-
-
+    
     var active = null;
-
-
-
+    
     var select = function (el) {
 
         if (!el || !el.stepData) {
@@ -384,33 +294,19 @@
             return false;
 
         }
-
-
-
+        
         // Sometimes it's possible to trigger focus on first link with some keyboard action.
 
         // Browser in such a case tries to scroll the page to make this element visible
 
         // (even that body overflow is set to hidden) and it breaks our careful positioning.
+        
+        // make the page scroll back to the top whenever slide is selected
 
-        //
-
-        // So, as a lousy (and lazy) workaround we will make the page scroll back to the top
-
-        // whenever slide is selected
-
-        //
-
-        // If you are reading this and know any better way to handle it, I'll be glad to hear about it!
-
-        window.scrollTo(0, 0);
-
-
-
+       window.scrollTo(0, 0);
+        
         var step = el.stepData;
-
-
-
+        
         if (active) {
 
             active.classList.remove("active");
@@ -418,21 +314,15 @@
         }
 
         el.classList.add("active");
-
-
-
+        
         impress.className = "step-" + el.id;
-
-
-
+        
         // `#/step-id` is used instead of `#step-id` to prevent default browser
 
         // scrolling to element in hash
 
         window.location.hash = "#/" + el.id;
-
-
-
+        
         var target = {
 
             rotate: {
@@ -466,19 +356,13 @@
             }
 
         };
-
-
-
+        
         var zoomin = target.scale.x >= current.scale.x;
-
-
-
+        
         css(impress, {
 
-            // to keep the perspective look similar for different scales
-
-            // we need to 'scale' the perspective, too
-
+            // to keep the perspective look similar for different scales, need to 'scale' the perspective
+            
             perspective: step.scale.x * 1000 + "px",
 
             transform: scale(target.scale),
@@ -486,9 +370,7 @@
             transitionDelay: (zoomin ? "500ms" : "0ms")
 
         });
-
-
-
+        
         css(canvas, {
 
             transform: rotate(target.rotate, true) + translate(target.translate),
@@ -496,25 +378,17 @@
             transitionDelay: (zoomin ? "0ms" : "500ms")
 
         });
-
-
-
+        
         current = target;
 
         active = el;
-
-
-
+        
         return el;
 
     }
-
-
-
+    
     // EVENTS
-
-
-
+    
     document.addEventListener("keydown", function (event) {
 
         if (event.keyCode == 9 || (event.keyCode >= 32 && event.keyCode <= 34) || (event.keyCode >= 37 && event.keyCode <= 40)) {
@@ -552,21 +426,15 @@
                     break;
 
             }
-
-
-
+            
             select(next);
-
-
-
+            
             event.preventDefault();
 
         }
 
     }, false);
-
-
-
+    
     document.addEventListener("click", function (event) {
 
         // event delegation with "bubbling"
@@ -584,15 +452,11 @@
             target = target.parentNode;
 
         }
-
-
-
+        
         if (target.tagName == "A") {
 
             var href = target.getAttribute("href");
-
-
-
+            
             // if it's a link to presentation step, target this step
 
             if (href && href[0] == '#') {
@@ -603,8 +467,7 @@
 
         }
 
-
-
+        
         if (select(target)) {
 
             event.preventDefault();
@@ -612,9 +475,7 @@
         }
 
     });
-
-
-
+    
     var getElementFromUrl = function () {
 
         // get id from url # by removing `#` or `#/` from the beginning,
@@ -624,23 +485,17 @@
         return byId(window.location.hash.replace(/^#\/?/, ""));
 
     }
-
-
-
+    
     window.addEventListener("hashchange", function () {
 
         select(getElementFromUrl());
 
     }, false);
-
-
-
+    
     // START 
 
     // by selecting step defined in url or first step of the presentation
 
     select(getElementFromUrl() || steps[0]);
-
-
 
 })(document, window);
